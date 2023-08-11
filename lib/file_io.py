aff3f4ebb384e51e51051e318d41711adbfd12d8
Shawn Webster <swolfgangwebster@gmail.com>
2023-08-11 03:12:17 -0500

    path

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+
+    l = open(str(file_name) + '.txt', mode='w', encoding='utf-8')
+    l.write(file_content)
+    l.close()
 
 def append_file(file_name, append_content):
-    pass
+    l = open(str(file_name) + '.txt', mode='a', encoding='utf-8')
+    l.write(append_content)
+    l.close()
 
 def read_file(file_name):
-    pass
+    l = open(str(file_name) + '.txt', mode='r', encoding='utf-8')
+    return l.read()
